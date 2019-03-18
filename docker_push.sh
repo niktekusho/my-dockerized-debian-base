2df851ceb85db54ef21eb9536c5eccdd66ed987d
niktekusho <nicola.dalmaso@outlook.com>
2019-03-18 14:46:21 +0100

    Fixed Travis CI build crash

--- docker_push.sh
+++ docker_push.sh
@@ -5,4 +5,8 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# Previous build step ensures the image is working
+docker build -t nikgatto/my-dockerized-debian-base .
+
+# Finally push the image to the Docker Hub
 docker push nikgatto/my-dockerized-debian-base:latest
