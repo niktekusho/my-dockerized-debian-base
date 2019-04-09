dc028a562738bdbac293a55b021a515da9097710
niktekusho <nicola.dalmaso@outlook.com>
2019-04-09 17:12:44 +0200

    Implement tagged releases

--- docker_push.sh
+++ docker_push.sh
@@ -5,10 +5,17 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-echo "Tag is: ${TRAVIS_TAG:-null}"
-
 # Previous build step ensures the image is working
 docker build -t nikgatto/my-dockerized-debian-base .
 
+# If the Travis tag env var is undefined then use latest
+if [ -z "${TRAVIS_TAG:-}" ]; then
+	PUBLISH_TAG="latest"
+else
+	PUBLISH_TAG=${TRAVIS_TAG}
+fi
+
+echo "Publish Tag is: ${PUBLISH_TAG}"
+
 # Finally push the image to the Docker Hub
-docker push nikgatto/my-dockerized-debian-base:latest
+docker push nikgatto/my-dockerized-debian-base:"${PUBLISH_TAG}"
